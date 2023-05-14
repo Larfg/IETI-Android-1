0c81913722a4f14413e25b930118e673e80b8ec5
larfg <luis.giraldo-r@mail.escuelaing.edu.co>
2023-05-14 17:30:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Log.d("MainActivity", "Hello World");
+        }
+        catch (Exception ex){
+            Log.e("Error",ex.getMessage());
+        }
+    }
+}
